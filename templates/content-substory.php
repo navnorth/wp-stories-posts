ee7f06b99e7bc71d9ef223c54cc1e834eb993013
John Paul Balagolan <johnpaulbalagolan@gmail.com>
2016-06-07 20:54:39 +0800

    update styling display of story list when there's no feature image

--- templates/content-substory.php
+++ templates/content-substory.php
@@ -54,54 +54,52 @@
             <img class="featured_item_image" src="<?php echo $img_url; ?>" alt="<?php echo $img_alt; ?>" />
         </div>
     <?php endif; ?>
-    <h4 class="substory_loc">
-    	<?php
-    		if(get_post_meta($post->ID, "story_district", true))
-			{
-				echo get_post_meta($post->ID, "story_district", true).', ';
-			}
-			$states = get_the_terms( $post->ID , 'state' );
-			if(isset($states) && !empty($states))
-			{
-				foreach($states as $state)
-				{
-					echo $state->name.' - ';
-					break;
-				}
-			}
-			$grades = get_the_terms( $post->ID , 'grade_level' );
-			if(isset($grades) && !empty($grades))
-			{
-				foreach($grades as $grade)
-				{
-					echo $grade->name;
-					break;
-				}
-			}
-		?>
-    </h4>
-    <p>
-        <?php
-		    $content = get_the_content($post->ID);
-		    echo substr($content, 0, 300).'...';
-        ?>
-    </p>
-
-	<?php
-		$topics = get_the_terms( $post->ID , 'story_tag' );
-		if(isset($topics) && !empty($topics))
-		{
-			$postedin = '<strong>'.__( 'Topic' , SCP_SLUG );
-            if (count($topics) > 1) $postedin .= 's';
-            $postedin .= ':</strong> ';
-
-			foreach($topics as $topic)
-			{
-				$termlink = get_term_link($topic->term_id, $topic->taxonomy);
-                $postedin .= '<a href="'.$termlink.'">'.$topic->name.'</a>, ';
-			}
-            echo '<p>' . trim($postedin,', ') . '</p>';
-		}
-	?>
-
+	<h4 class="substory_loc">
+	    <?php
+		    if(get_post_meta($post->ID, "story_district", true))
+			    {
+				    echo get_post_meta($post->ID, "story_district", true).', ';
+			    }
+			    $states = get_the_terms( $post->ID , 'state' );
+			    if(isset($states) && !empty($states))
+			    {
+				    foreach($states as $state)
+				    {
+					    echo $state->name.' - ';
+					    break;
+				    }
+			    }
+			    $grades = get_the_terms( $post->ID , 'grade_level' );
+			    if(isset($grades) && !empty($grades))
+			    {
+				    foreach($grades as $grade)
+				    {
+					    echo $grade->name;
+					    break;
+				    }
+			    }
+		    ?>
+	</h4>
+	<p>
+	    <?php
+			$content = get_the_content($post->ID);
+			echo substr($content, 0, 300).'...';
+	    ?>
+	</p>
+	    <?php
+		    $topics = get_the_terms( $post->ID , 'story_tag' );
+		    if(isset($topics) && !empty($topics))
+		    {
+			    $postedin = '<strong>'.__( 'Topic' , SCP_SLUG );
+		if (count($topics) > 1) $postedin .= 's';
+		$postedin .= ':</strong> ';
+    
+			    foreach($topics as $topic)
+			    {
+				    $termlink = get_term_link($topic->term_id, $topic->taxonomy);
+		    $postedin .= '<a href="'.$termlink.'">'.$topic->name.'</a>, ';
+			    }
+		echo '<p>' . trim($postedin,', ') . '</p>';
+		    }
+	    ?>
 </div>

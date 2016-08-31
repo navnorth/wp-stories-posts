87f14eb3127bae0e4ab56eb60ba7d7cfd92d3498
joe hobson <joe@navigationnorth.com>
2016-08-31 11:42:10 -0500

    version bump

--- stories-custom-post-type.php
+++ stories-custom-post-type.php
@@ -3,7 +3,7 @@
  Plugin Name: Story Custom Post Type
  Plugin URI: http://www.navigationnorth.com/wordpress/stories-plugin
  Description: Stories as a custom post type, with custom metadata and display. Developed in collaboration with Monad Infotech (http://monadinfotech.com)
- Version: 0.5.0
+ Version: 0.5.1
  Author: Navigation North
  Author URI: http://www.navigationnorth.com
  Text Domain: wp-stories-posts
@@ -36,7 +36,7 @@ define( 'SCP_SLUG','wp-stories-posts' );
 define( 'SCP_FILE',__FILE__);
 define( 'SCP_PLUGIN_NAME' , 'Story Custom Post Type' );
 define( 'SCP_PLUGIN_INFO' , '#' );
-define( 'SCP_VERSION' , '0.5.0');
+define( 'SCP_VERSION' , '0.5.1');
 define( 'GOOGLE_API_KEY' , 'AIzaSyACobLJYn3xWIaxrZHEa6G3VjOteYpWBno');
 
 include_once(SCP_PATH.'init.php');
@@ -584,7 +584,7 @@ function setup_settings_form() {
 				'description' => __('necessary for displaying map', SCP_SLUG)
 			)
 			   );
-	
+
 	/* Enable/Disable State Selection */
 	add_settings_field(
 			'enable_state',

9b51bc1a651e882fd9f4fecf35951843371953ef
John Paul Balagolan <johnpaulbalagolan@gmail.com>
2017-02-10 00:39:02 +0800

    add vimeo video background to story embed

--- includes/functions.php
+++ includes/functions.php
@@ -25,8 +25,11 @@ function get_background($id) {
         $bg_url = wp_get_attachment_image_src(get_post_thumbnail_id($id), 'full');
         $background_image_url = $bg_url[0];
     } elseif(has_youtube_video($id)){
-        $youtubeID = get_youtubeID($id);
+        $youtubeID = get_videoID($id);
         $background_image_url = get_youtube_image($youtubeID);
+    } elseif(has_vimeo_video($id)){
+        $vimeoID = get_videoID($id);
+        $background_image_url = get_vimeo_image($vimeoID);
     } else {
         $background_image_url = SCP_URL . "images/top_strap_img.jpg";
     }
@@ -35,28 +38,42 @@ function get_background($id) {
 }
 
 /**
- * Checks if story has embedded video
+ * Checks if story has youtube video
  **/
 function has_youtube_video($id) {
     $has_video = false;
     
-    $video = get_post_meta($id,'story_video',true);
+    $video = get_post_meta($id,'story_video_host',true);
     
-    if (strlen($video)>0){
+    if ($video=="1"){
         $has_video = true;
     }
     return $has_video;
 }
 
 /**
+ * Checks if story has vimeo video
+ **/
+function has_vimeo_video($id){
+    $has_vimeo = false;
+    
+    $video = get_post_meta($id,'story_video_host',true);
+    
+    if ($video=="2"){
+        $has_vimeo = true;
+    }
+    return $has_vimeo;
+}
+
+/**
  * Get Youtube ID from embedded code
  **/
-function get_youtubeID($id) {
-    $youtubeID = null;
+function get_videoID($id) {
+    $videoID = null;
     
-    $youtubeID = get_post_meta( $id, 'story_video', true);
+    $videoID = get_post_meta( $id, 'story_video', true);
     
-    return $youtubeID;
+    return $videoID;
 }
 
 /**
@@ -65,6 +82,19 @@ function get_youtubeID($id) {
 function get_youtube_image($youtube_id) {
     $youtube_url = "//img.youtube.com/vi/$youtube_id/maxresdefault.jpg";
     return $youtube_url;
+}
+
+/**
+ * Get Vimeo Image
+ **/
+function get_vimeo_image($vimeo_id) {
+    $vimeo = unserialize(file_get_contents("http://vimeo.com/api/v2/video/$vimeo_id.php"));
+    
+    $vimeo_url = $vimeo[0]['thumbnail_large'];
+    
+    $vimeo_url = str_replace( "_640", "", $vimeo_url );
+    
+    return $vimeo_url;
 }
 
 /**

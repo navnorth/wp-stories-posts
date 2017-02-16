31eef011d2cb3b99b77ff3014fabe8521c031b53
John Paul Balagolan <johnpaulbalagolan@gmail.com>
2017-02-17 00:27:42 +0800

    debug wp-load file location

--- widgets/embed/embed.php
+++ widgets/embed/embed.php
@@ -1,7 +1,7 @@
 <?php
 
 $cur_dir = dirname($_SERVER["SCRIPT_FILENAME"]);
-var_dump($cur_dir);
+var_dump(file_exists($cur_dir.'/../../../../../wp-load.php'));
 
 //load WordPress
 if (file_exists($cur_dir.'/../../../../../wp-load.php'))

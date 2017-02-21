c020e44b629759f1cc4a66fb84297988cfa4c83b
John Paul Balagolan <johnpaulbalagolan@gmail.com>
2017-02-21 23:06:58 +0800

    add debugging

--- js/vimeo.ga.js
+++ js/vimeo.ga.js
@@ -134,7 +134,7 @@ var vimeoGAJS = (window.vimeoGAJS) ? window.vimeoGAJS : {};
 
       // Source URL
       var iframeSrc;
-      
+      console.log(iframe);
       if ($(iframe).attr('src').indexOf('?')>-1) {
         iframeSrc = $(iframe).attr('src').split('?')[0];
       }
